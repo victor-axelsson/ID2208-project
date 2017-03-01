3f8c5e3db2549b360d18f0061583dbff13963a08
RubbyRedson <gurnik2009@gmail.com>
2017-03-01 15:15:40 +0100

    Switched place for ins and outs comparison added

--- Complementary Files/project/src/se/kth/webservice/project/parsing/XMLFileHandler.java
+++ Complementary Files/project/src/se/kth/webservice/project/parsing/XMLFileHandler.java
@@ -184,9 +184,9 @@ public class XMLFileHandler {
             modelMappings.add(model);
         }
 
-        saveCache();
+        //saveCache();
 
-        System.out.println("asd");
+        //System.out.println("asd");
     }
 
     private void saveCache(){
@@ -389,6 +389,7 @@ public class XMLFileHandler {
         for (int i = 0; i < modelMappings.size() -1;  i++){
             for(int j = i +1; j < modelMappings.size(); j++){
                 onCompare.compare(modelMappings.get(i), modelMappings.get(j));
+                onCompare.compare(modelMappings.get(j), modelMappings.get(i));
             }
 
             System.out.println("---- Done with comparing doc: " + (i +1) + "/" + (modelMappings.size() -1) + " ----");
